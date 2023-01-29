49a217543478acc70bd5ff3fd46ba507ac834d19
RajAnubhav <anubhavraj742@gmail.com>
2023-01-30 00:51:16 +0530

    updating FLIPCARDS.java

--- CODECHEF/2023/1. JAN/28/FLIPCARDS.java
+++ CODECHEF/2023/1. JAN/28/FLIPCARDS.java
@@ -1,16 +1,20 @@
 import java.util.Scanner;
+
 public class FLIPCARDS {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        while(t-->0){
+        while (t-- > 0) {
             int n, x;
             n = sc.nextInt();
             x = sc.nextInt();
-            if(n>x)
-                System.out.println(x);
-            else    
+
+            if (x < 0 || x > n || n < 2 || n > 100)
                 System.out.println(0);
+            if (n - x >= x)
+                System.out.println(x);
+            else
+                System.out.println(n - x);
         }
         sc.close();
     }
